25996fdc5d8af04bf128803cb54065082cb2e6a2
Chew Esmero <chew.esmero@mobingi.com>
2017-07-05 11:40:11 +0900

    Initial files, include vendor.

---
+++ cmd/root.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "mocli",
+	Short: "",
+	Long:  `Mobingi API command line interface.`,
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Println(err)
+		os.Exit(-1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/mobingilabs/mocli/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
