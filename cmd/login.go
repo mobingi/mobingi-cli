17bc5450759fc93924f93703d3b60aab3f103c0c
chew <chew.esmero@mobingi.com>
2017-07-06 15:54:59 +0900

    Try home folder detect and pass input in all platforms.

--- cmd/login.go
+++ cmd/login.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"io/ioutil"
 	"log"
 
+	"github.com/mitchellh/go-homedir"
+	"github.com/mobingilabs/mocli/pkg/util"
 	"github.com/spf13/cobra"
 )
 
@@ -10,14 +13,30 @@ var loginCmd = &cobra.Command{
 	Use:   "login",
 	Short: "",
 	Long:  `Placeholder for the documentation.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		log.Println("login here")
-	},
+	Run:   login,
 }
 
 func init() {
 	rootCmd.AddCommand(loginCmd)
 	loginCmd.Flags().StringP("client-id", "i", "", "client id")
 	loginCmd.Flags().StringP("client-secret", "s", "", "client secret")
-	loginCmd.Flags().StringP("grant-type", "g", "client_credentials", "grant type")
+	loginCmd.Flags().StringP("grant-type", "g", "client_credentials", "grant type (valid values: 'client_credentials', 'password')")
+}
+
+func login(cmd *cobra.Command, args []string) {
+	log.Println("login here")
+	hd, _ := homedir.Dir()
+	log.Println("home:", hd)
+	cred := hd + `/.mocli/credentials`
+
+	token, err := ioutil.ReadFile(cred)
+	if err != nil {
+		log.Println(err)
+		ioutil.WriteFile(hd+`/.mocli/credentials`, []byte("hello"), 0644)
+	}
+
+	log.Println(string(token))
+
+	user, pass := util.GetUserPassword()
+	log.Println(user, pass)
 }
--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,1 +1,20 @@
 package util
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"syscall"
+
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+func GetUserPassword() (string, string) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Username: ")
+	user, _ := reader.ReadString('\n')
+	fmt.Print("Password: ")
+	pass, _ := terminal.ReadPassword(int(syscall.Stdin))
+	return strings.TrimSpace(user), strings.TrimSpace(string(pass))
+}
