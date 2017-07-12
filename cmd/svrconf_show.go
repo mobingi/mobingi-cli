acc29b67b053cdee81c18ad817908daab015d06e
chew <chew.esmero@mobingi.com>
2017-07-12 16:03:54 +0900

    Add cmd to show serverconfig.

--- cmd/svrconf_show.go
+++ cmd/svrconf_show.go
@@ -1,11 +1,13 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/mobingilabs/mocli/pkg/cli"
+	"github.com/mobingilabs/mocli/pkg/svrconf"
 	"github.com/mobingilabs/mocli/pkg/util"
 	"github.com/spf13/cobra"
 )
@@ -58,82 +60,37 @@ func show(cmd *cobra.Command, args []string) {
 		return
 	}
 
-	_ = resp
 	if pfmt == "json" || pfmt == "" {
-	}
-
-	/*
-		var stacks []stack.ListStack
-		err = json.Unmarshal(body, &stacks)
+		var sc svrconf.ServerConfig
+		err = json.Unmarshal(body, &sc)
 		if err != nil {
+			log.Println(err)
 			var m map[string]interface{}
 			err = json.Unmarshal(body, &m)
 			if err != nil {
-				util.ErrorExit("Internal error.", 1)
+				util.ErrorExit("internal error", 1)
 			}
 
 			serr := util.ResponseError(resp, m)
 			if serr != "" {
+				log.Println("error?")
 				util.ErrorExit(serr, 1)
 			}
 		}
 
-		switch util.GetCliStringFlag(cmd, "fmt") {
-		case "min":
-			stbl := term.NewTable(0, 10, 5, ' ', 0)
-			fmt.Fprintf(stbl, "STACK ID\tSTACK NAME\tPLATFORM\tSTATUS\tREGION\tLAUNCHED\n")
-			for _, s := range stacks {
-				timestr := s.CreateTime
-				t, err := time.Parse(time.RFC3339, s.CreateTime)
-				if err == nil {
-					timestr = t.Format(time.RFC1123)
-				}
+		indent := util.GetCliIntFlag(cmd, "indent")
+		mi, err := json.MarshalIndent(sc, "", util.Indent(indent))
+		if err != nil {
+			util.ErrorExit(err.Error(), 1)
+		}
 
-				platform := "?"
-				if s.Configuration.AWS != "" {
-					platform = "AWS"
-				}
-
-				fmt.Fprintf(stbl, "%s\t%s\t%s\t%s\t%s\t%s\n", s.StackId, s.Nickname, platform, s.StackStatus, s.Configuration.Region, timestr)
-			}
-
-			term.Print(stbl)
-			term.Flush()
-		case "text":
-			indent := util.GetCliIntFlag(cmd, "indent")
-			stack.PrintR(os.Stdout, &stacks[0], 0, indent)
-			f := util.GetCliStringFlag(cmd, "out")
-			if f != "" {
-				fp, err := os.Create(f)
-				if err != nil {
-					util.ErrorExit(err.Error(), 1)
-				}
-
-				defer fp.Close()
-				w := bufio.NewWriter(fp)
-				defer w.Flush()
-				stack.PrintR(w, &stacks[0], 0, indent)
-				log.Println(fmt.Sprintf("Output written to %s.", f))
-			}
-		case "json":
-			indent := util.GetCliIntFlag(cmd, "indent")
-			mi, err := json.MarshalIndent(stacks, "", util.Indent(indent))
+		fmt.Println(string(mi))
+		out := util.GetCliStringFlag(cmd, "out")
+		if out != "" {
+			err = util.WriteToFile(out, mi)
 			if err != nil {
 				util.ErrorExit(err.Error(), 1)
 			}
-
-			// this should be a prettified JSON output
-			fmt.Println(string(mi))
-
-			f := util.GetCliStringFlag(cmd, "out")
-			if f != "" {
-				err = ioutil.WriteFile(f, mi, 0644)
-				if err != nil {
-					util.ErrorExit(err.Error(), 1)
-				}
-
-				log.Println(fmt.Sprintf("Output written to %s.", f))
-			}
 		}
-	*/
+	}
 }
---
+++ pkg/svrconf/svrconf.go
@@ -0,0 +1,10 @@
+package svrconf
+
+type ServerConfig struct {
+	Code    string `json:"code,omitempty"`
+	CodeDir string `json:"codeDir,omitempty"`
+	Image   string `json:"image,omitempty"`
+	Ports   []int  `json:"ports,omitempty"`
+	Updated int64  `json:"updated,omitempty"`
+	Version string `json:"version,omitempty"`
+}
