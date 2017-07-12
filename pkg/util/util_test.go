2db381924ae57432bb6a9b29e93dfd13de2e8643
chew <chew.esmero@mobingi.com>
2017-07-12 16:35:25 +0900

    Add more tests for util.

--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -1,10 +1,35 @@
 package util
 
-import "testing"
+import (
+	"net/http"
+	"testing"
+)
 
 func TestIndent(t *testing.T) {
-	i := Indent(4)
-	if i != "    " {
+	if i := Indent(4); i != "    " {
 		t.Errorf("Expected four(4) whitespaces, got %v", i)
+	}
+}
+
+func TestResponseError(t *testing.T) {
+	r := &http.Response{
+		StatusCode: 401,
+	}
+
+	m := make(map[string]interface{})
+	if re := ResponseError(r, m); re == "" {
+		t.Errorf("Expected error mesage, got %v", re)
+	}
+
+	r.StatusCode = 200
+	if re := ResponseError(r, m); re != "" {
+		t.Errorf("Expected empty mesage, got %v", re)
+	}
+
+	m["code"] = 100
+	m["error"] = "error string"
+	m["message"] = "error message"
+	if re := ResponseError(r, m); re == "" {
+		t.Errorf("Expected error mesage, got %v", re)
 	}
 }
