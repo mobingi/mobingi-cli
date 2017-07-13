d9012cbc8c06f88429e75231672d244b942e1aa3
chew <chew.esmero@mobingi.com>
2017-07-13 16:02:30 +0900

    Add more test cases.

--- cmd/svrconf_update_test.go
+++ cmd/svrconf_update_test.go
@@ -12,4 +12,28 @@ func TestBuildPayload(t *testing.T) {
 	if err != nil {
 		t.Errorf("Expected proper marshaling, got error %v", err)
 	}
+
+	v = buildPayload("sid", "KEY:value")
+	m = json.RawMessage(v)
+	_, err = json.Marshal(&m)
+	if err != nil {
+		t.Errorf("Expected proper marshaling, got error %v", err)
+	}
+
+	v = buildPayload("sid", "KEY1:value1,KEY2:value2")
+	m = json.RawMessage(v)
+	_, err = json.Marshal(&m)
+	if err != nil {
+		t.Errorf("Expected proper marshaling, got error %v", err)
+	}
+
+	v = buildPayload("sid", "")
+	if v != "" {
+		t.Errorf("Expected an empty string, got %s", v)
+	}
+
+	v = buildPayload("sid", "KEY1,value1,KEY2,value2")
+	if v != "" {
+		t.Errorf("Expected an empty string, got %s", v)
+	}
 }
