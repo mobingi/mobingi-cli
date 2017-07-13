21b4f45a47a9f96601c215ff21242ff3a880a561
chew <chew.esmero@mobingi.com>
2017-07-13 17:55:42 +0900

    Add additional fields to serverconfig struct.

--- pkg/svrconf/svrconf.go
+++ pkg/svrconf/svrconf.go
@@ -1,10 +1,12 @@
 package svrconf
 
 type ServerConfig struct {
-	Code    string `json:"code,omitempty"`
-	CodeDir string `json:"codeDir,omitempty"`
-	Image   string `json:"image,omitempty"`
-	Ports   []int  `json:"ports,omitempty"`
-	Updated int64  `json:"updated,omitempty"`
-	Version string `json:"version,omitempty"`
+	Code          string `json:"code,omitempty"`
+	CodeDir       string `json:"codeDir,omitempty"`
+	Image         string `json:"image,omitempty"`
+	Ports         []int  `json:"ports,omitempty"`
+	Updated       int64  `json:"updated,omitempty"`
+	Version       string `json:"version,omitempty"`
+	GitPrivateKey string `json:"gitPrivateKey,omitempty"`
+	GitReference  string `json:"gitReference,omitempty"`
 }
