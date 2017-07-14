1787b64c7bdada57ce075ba8ac17280b2c10b8ce
chew <chew.esmero@mobingi.com>
2017-07-14 15:09:22 +0900

    Separate test files.

---
+++ pkg/util/error_test.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestResponseError(t *testing.T) {
+	r := &http.Response{
+		StatusCode: 401,
+	}
+
+	b := []byte(`{}`)
+	if re := ResponseError(r, b); re == "" {
+		t.Errorf("Expected error mesage, got %v", re)
+	}
+
+	r.StatusCode = 200
+	if re := ResponseError(r, b); re != "" {
+		t.Errorf("Expected empty mesage, got %v", re)
+	}
+
+	b = []byte(`{"code":"100","error":"error":"message":"error message"}`)
+	if re := ResponseError(r, b); re == "" {
+		t.Errorf("Expected error mesage, got %v", re)
+	}
+}
--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -1,33 +1,11 @@
 package util
 
 import (
-	"net/http"
 	"testing"
 )
 
 func TestIndent(t *testing.T) {
 	if i := Indent(4); i != "    " {
 		t.Errorf("Expected four(4) whitespaces, got %v", i)
-	}
-}
-
-func TestResponseError(t *testing.T) {
-	r := &http.Response{
-		StatusCode: 401,
-	}
-
-	b := []byte(`{}`)
-	if re := ResponseError(r, b); re == "" {
-		t.Errorf("Expected error mesage, got %v", re)
-	}
-
-	r.StatusCode = 200
-	if re := ResponseError(r, b); re != "" {
-		t.Errorf("Expected empty mesage, got %v", re)
-	}
-
-	b = []byte(`{"code":"100","error":"error":"message":"error message"}`)
-	if re := ResponseError(r, b); re == "" {
-		t.Errorf("Expected error mesage, got %v", re)
 	}
 }
