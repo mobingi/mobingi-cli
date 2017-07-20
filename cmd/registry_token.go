fe41b065fa7771a9e8f0e3c77007d88e2cf29ad8
chew <chew.esmero@mobingi.com>
2017-07-20 17:34:02 +0900

    Parameterize token params.

--- cmd/registry_token.go
+++ cmd/registry_token.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 
@@ -20,11 +19,15 @@ var tokenCmd = &cobra.Command{
 
 func init() {
 	registryCmd.AddCommand(tokenCmd)
+	tokenCmd.Flags().String("account", "", "subuser name")
 	tokenCmd.Flags().String("username", "", "username (account subuser)")
 	tokenCmd.Flags().String("password", "", "password (account subuser)")
+	tokenCmd.Flags().String("service", "Mobingi Docker Registry", "service for authentication")
+	tokenCmd.Flags().String("scope", "", "scope for authentication")
 }
 
 func token(cmd *cobra.Command, args []string) {
+	passin := false
 	user := util.GetCliStringFlag(cmd, "username")
 	pass := util.GetCliStringFlag(cmd, "password")
 	if user == "" {
@@ -37,57 +40,44 @@ func token(cmd *cobra.Command, args []string) {
 
 	if pass == "" {
 		pass = util.Password()
+		passin = true
 	}
 
 	if pass == "" {
 		util.CheckErrorExit("password cannot be empty", 1)
 	}
 
-	fmt.Println("\n") // new line after the password input
-	log.Println(user, pass)
-
-	/*
-		data := url.Values{}
-		data.Add("account", "chewsubuser1")
-		// data.Add("username", "chewsubuser1")
-		// data.Add("password", "mobingi")
-		data.Add("service", "Mobingi Docker Registry")
-		data.Add("scope", "registry:chewsubuser1:catalog:*")
-	*/
+	if passin {
+		fmt.Println("\n") // new line after the password input
+	}
 
 	var Url *url.URL
-	Url, err := url.Parse("https://apidev.mobingi.com")
+	Url, err := url.Parse(util.GetCliStringFlag(cmd, "url"))
 	if err != nil {
 		util.CheckErrorExit(err, 1)
 	}
 
-	Url.Path += "/v2/docker/token"
+	Url.Path += "/" + util.GetCliStringFlag(cmd, "apiver") + "/docker/token"
 	parameters := url.Values{}
-	parameters.Add("account", "chewsubuser1")
-	parameters.Add("service", "Mobingi Docker Registry")
-	parameters.Add("scope", "repository:chewsubuser1/hello:*")
-	// parameters.Add("scope", "registry:catalog:*")
+	parameters.Add("account", util.GetCliStringFlag(cmd, "account"))
+	parameters.Add("service", util.GetCliStringFlag(cmd, "service"))
+	parameters.Add("scope", util.GetCliStringFlag(cmd, "scope"))
 	Url.RawQuery = parameters.Encode()
 
-	fmt.Printf("Encoded URL is %q\n", Url.String())
-
 	client := &http.Client{}
-	// req, err := http.NewRequest("GET", "https://apidev.mobingi.com/v2/docker/token", strings.NewReader(data.Encode()))
 	req, err := http.NewRequest("GET", Url.String(), nil)
 	req.SetBasicAuth(user, pass)
-	log.Println(req.URL.String())
-
 	resp, err := client.Do(req)
 	if err != nil {
 		util.CheckErrorExit(err, 1)
 	}
 
 	defer resp.Body.Close()
-	log.Println(resp)
+	// log.Println(resp)
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		util.CheckErrorExit(err, 1)
 	}
 
-	fmt.Println("body:", string(body))
+	fmt.Println(string(body))
 }
