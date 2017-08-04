e8346c283d0a8d8d3f67a864a9d2bc354e637418
flowerinthenight <chew.esmero@mobingi.com>
2017-08-04 18:20:18 +0900

    Transfer debug check to error printer.

--- pkg/debug/debug.go
+++ pkg/debug/debug.go
@@ -5,6 +5,9 @@ import (
 	"log"
 
 	"github.com/fatih/color"
+	"github.com/mobingilabs/mocli/pkg/cli/confmap"
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
 )
 
 var Verbose bool
@@ -21,4 +24,11 @@ func Error(v ...interface{}) {
 	red := color.New(color.FgRed).SprintFunc()
 	m := fmt.Sprintln(v...)
 	log.Print(fmt.Sprintf("%s %s", red("error:"), m))
+
+	// stack trace from 'errors'
+	if viper.GetBool(confmap.ConfigKey("debug")) {
+		err := fmt.Errorf(m)
+		err = errors.WithStack(err)
+		fmt.Printf("%+v\n", err)
+	}
 }
--- pkg/debug/error.go
+++ pkg/debug/error.go
@@ -1,42 +1,21 @@
 package debug
 
 import (
-	"encoding/json"
-	"fmt"
 	"os"
-
-	"github.com/mobingilabs/mocli/pkg/cli/confmap"
-	"github.com/parnurzeal/gorequest"
-	"github.com/pkg/errors"
-	"github.com/spf13/viper"
 )
 
 func isError(err interface{}) bool {
-	var (
-		derr  error
-		valid bool
-	)
-
+	var valid bool
 	switch err.(type) {
 	case string:
 		if err != "" {
-			derr = errors.WithStack(fmt.Errorf(fmt.Sprintf("%s", err)))
 			Error(err)
 			valid = true
 		}
 	case error:
 		if err != nil {
-			e, _ := err.(error)
-			derr = errors.WithStack(e)
 			Error(err)
 			valid = true
-		}
-	}
-
-	if valid {
-		if viper.GetBool(confmap.ConfigKey("debug")) {
-			// stack trace from 'errors'
-			fmt.Printf("%+v\n", derr)
 		}
 	}
 
@@ -56,40 +35,4 @@ func IsHttpSuccess(code int) bool {
 	}
 
 	return false
-}
-
-func ResponseError(r gorequest.Response, b []byte) string {
-	errcnt := 0
-	var m map[string]interface{}
-	err := json.Unmarshal(b, &m)
-	if err != nil {
-		return err.Error()
-	}
-
-	var serr, cem string
-	if r.StatusCode != 200 {
-		serr = serr + "[" + r.Status + "]"
-	}
-
-	// these three should be present to be considered error
-	if c, found := m["code"]; found {
-		cem = cem + "[" + fmt.Sprintf("%s", c) + "]"
-		errcnt += 1
-	}
-
-	if e, found := m["error"]; found {
-		cem = cem + fmt.Sprintf(" %s:", e)
-		errcnt += 1
-	}
-
-	if s, found := m["message"]; found {
-		cem = cem + fmt.Sprintf(" %s", s)
-		errcnt += 1
-	}
-
-	if errcnt == 3 {
-		serr = serr + cem
-	}
-
-	return serr
 }
