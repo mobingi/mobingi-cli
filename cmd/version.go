16b09fc6f18475ea16326423334066c4db07fad4
flowerinthenight <chew.esmero@gmail.com>
2017-12-26 04:47:34 +0000

    Auto version.

--- cmd/version.go
+++ cmd/version.go
@@ -6,13 +6,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var version = "local"
+
 func VersionCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:   "version",
 		Short: "print the version",
 		Long:  `Print the version.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("v1.0.8")
+			fmt.Println(version)
 		},
 	}
 }
